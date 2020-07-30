a0ec48732b85606e42c8efd309d1fc307c703cb6
Don Kats <donnykats@gmail.com>
2020-07-30 23:43:52 +0200

    react router and two pages added

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const About = props => {
+  return (
+    <div>
+      <h3>About View</h3>
+      <p> This is the about view of SPA</p>
+    </div>
+  );
+};
+
+export default About;
