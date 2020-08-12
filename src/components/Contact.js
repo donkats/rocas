f1babd945351da5522f063c52f0ccca5d34dffa8
Don Kats <donnykats@gmail.com>
2020-08-12 17:41:07 +0200

    added form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,6 +14,22 @@ const Contact = props => {
             
             <p>U kunt contact met ons opnemen door onderstaande contactformulier in te vullen. Wij zullen dan zo
             snel mogelijk reageren.</p>
+
+            <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Naam: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>E-mailadres: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Bericht: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Verzenden</button>
+          </p>
+        </form>
           </div>
         </Cell>
       </Grid>
